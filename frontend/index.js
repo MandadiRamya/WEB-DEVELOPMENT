f75a3b1ad60691dd5ba0b363d6bb7db795340d9d
Mandadi Ramya <ramyamandadi8@gmail.com>
2026-08-13 16:31:48 +0530

    card

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,29 @@
 //alert("Welcome to web bootcamp!");
+
+var users = [
+    {
+        "name": "John Doe", 
+        "gender": "Male",
+        "image": "john.png"
+    },
+    {
+        "name": "Jane Doe", 
+        "gender": "Female",
+        "image": "jane.png"
+    }
+];
+
+var curID = 0;
+
+function toggleUser() {
+    curID = (curID + 1) % 2;
+
+    var username = document.getElementById("user-name");
+    var usergender = document.getElementById("user-gender");
+    var userimage = document.getElementById("user-image");
+
+    username.innerHTML = users[curID].name;
+    usergender.innerHTML = users[curID].gender;
+    userimage.src = users[curID].image;
+}
+
