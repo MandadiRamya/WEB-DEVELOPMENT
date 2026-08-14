76c69c2f705c431ad5aaf3ac163d2d0c77d0b173
Mandadi Ramya <ramyamandadi8@gmail.com>
2026-08-14 10:43:51 +0530

    keers

--- frontend/index.js
+++ frontend/index.js
@@ -27,4 +27,25 @@ function toggleUser() {
     userimage.src = users[curID].image;
 }
 
+function randomUser() {
+    fetch("https://randomuser.me/api/")
+        .then(function(res){
+            return res.json();
+        })
+        .then(function(data){
+            var username = document.getElementById("user-name");
+            var usergender = document.getElementById("user-gender");
+            var userimage = document.getElementById("user-image");
 
+            var newusername = data.results[0].name.first + " " + data.results[0].name.last;
+            var newusergender = data.results[0].gender;
+            var newuserimage = data.results[0].picture.large;
+
+            username.innerHTML = newusername;
+            usergender.innerHTML = newusergender;
+            userimage.src = newuserimage;
+        })
+        .catch(function(err){
+            console.log("Error occured : " + err);
+        })
+}
